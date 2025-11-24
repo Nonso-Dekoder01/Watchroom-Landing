09d140d21955ab31ec31d5d8c78f72fe8b0e5b54
Nonso-Dekoder01 <chinonsosolomon2007@gmail.com>
2025-11-24 16:44:07 +0100

    second commit - navbar complete

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
-const landing = () => {
+const Landing = () => {
   return (
-    <div>landing</div>
+    <div>
+
+        {/* NAVBAR */}
+        <nav className='w-full h-auto flex justify-between items-center py-5 px-5 md:px-10 lg:px-20'>
+            <div>LOGO</div>
+            <button className='bg-blue-800 py-3 px-6 rounded-md font-semibold text-sm'>Sign In</button>
+        </nav>
+
+
+
+    </div>
   )
 }
 
-export default landing
+export default Landing
